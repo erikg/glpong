c39a79d2df9d0a71016a38da5e8cdc31ab4665c4
erik <erik>
2003-06-19 15:03:57 +0000

    license header and RCS Id tag

--- src/game.h
+++ src/game.h
@@ -1,3 +1,26 @@
+/*****************************************************************************
+ * glpong - opengl/sdl game of the original video game; pong.                *
+ * Copyright (C) 2001-2003 Erik Greenwald <erik@smluc.org>                   *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this program; if not, write to the Free Software               *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *
+ ****************************************************************************/
+
+/*
+ * $Id: game.h,v 1.2 2003/06/19 15:03:57 erik Exp $ 
+ */
+
 #ifndef __GAME_H_
 #define __GAME_H_
 
