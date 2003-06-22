270abbd807c327983c419f38a5d1e379a940f5b5
erik <erik>
2003-06-22 22:48:20 +0000

    added license info

--- src/text.h
+++ src/text.h
@@ -1,3 +1,35 @@
+
+/*****************************************************************************
+ * glpong - opengl/sdl/openal pong game                                      *
+ * Copyright (C) 2003 Erik Greenwald <erik@smluc.org>                        *
+ *                                                                           *
+ * This program takes a directory as an argument, then walks through the     *
+ * directory looking for duplicate and partially duplicate files. If it      *
+ * an exact duplicate, it deletes one and creates a hardlink to the other,   *
+ * minimizing disk usage. If it finds a pair of files where they contain the *
+ * same data up to the size of the smaller file, it will prompt if you want  *
+ * to combine them. If you say yes, it will delete the smaller of the files  *
+ * and hardlink to the larger.                                               *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      * 
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this program; if not, write to the Free Software               *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *
+ ****************************************************************************/
+
+/*
+ * $Id: text.h,v 1.3 2003/06/22 22:48:20 erik Exp $
+ */
+
 /*****************************************************************************
  * glpong - opengl/sdl game of the original video game; pong.                *
  * Copyright (C) 2001-2003 Erik Greenwald <erik@smluc.org>                   *
@@ -18,7 +50,7 @@
  ****************************************************************************/
 
 /*
- * $Id: text.h,v 1.2 2003/06/19 15:03:57 erik Exp $ 
+ * $Id: text.h,v 1.3 2003/06/22 22:48:20 erik Exp $ 
  */
 
 #ifndef __TEXT_H_
