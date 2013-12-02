051fdbe5e990658630dcaf16cc7a1acf9a516e11
Erik Greenwald <erik@elfga.com>
2013-12-02 18:00:43 -0500

    update copyright

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 
 /*****************************************************************************
  * glpong - opengl/sdl game of the original video game; pong.                *
- * Copyright (C) 2001-2012 Erik Greenwald <erik@elfga.com>                   *
+ * Copyright (C) 2001-2013 Erik Greenwald <erik@elfga.com>                   *
  *                                                                           *
  * This program is free software; you can redistribute it and/or modify      *
  * it under the terms of the GNU General Public License as published by      *
@@ -75,7 +75,7 @@ int
 doversion (char *name)
 {
     printf ("\
-%s (%s) Copyright (C) 2001-2012 Erik Greenwald <erik@elfga.com>\n\
+%s (%s) Copyright (C) 2001-2013 Erik Greenwald <erik@elfga.com>\n\
 %s comes with ABSOLUTELY NO WARRANTY. Please read the GPL for details.\n\n", name, PACKAGE, VERSION);
     return 0;
 }
@@ -146,7 +146,7 @@ main (int argc, char **argv)
     atexit (SDL_Quit);
 
     SDL_ShowCursor (0);
-    SDL_WM_SetCaption ("glpong (C) 2001-2012 Erik Greenwald", "glpong");
+    SDL_WM_SetCaption ("glpong (C) 2001-2013 Erik Greenwald", "glpong");
 
     g = game_init ();
 
